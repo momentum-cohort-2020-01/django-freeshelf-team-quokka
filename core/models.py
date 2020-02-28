fd5e7b68b5c6e94264c0559c3b136cce690be767
aselkg <aselekkg@gmail.com>
2020-02-28 13:46:14 -0500

    removed created_date

--- core/models.py
+++ core/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils import timezone
+
 
 # Create your models here.
 class Book(models.Model):
@@ -7,7 +7,7 @@ class Book(models.Model):
     author = models.CharField(max_length=80)
     url = models.URLField(max_length=250)
     description = models.TextField(max_length=300)
-    created_date = models.DateTimeField(default=timezone.now)
+    
 
     def __str__ (self):
         return f"Book title: {self.title} author: {self.author} description: {self.description} "
