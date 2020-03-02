78d871e1995d1f9b367118b309e756811a6f0436
aselkg <aselekkg@gmail.com>
2020-03-02 15:27:05 -0500

    register page

--- books/urls.py
+++ books/urls.py
@@ -19,13 +19,14 @@ from django.urls import path, include
 from core import views
 
 urlpatterns = [
+    path('accounts/', include('registration.backends.default.urls')),
     path('admin/', admin.site.urls),
     path('', views.base, name='base'),
     path('books/', views.books_list, name='books_list'),
     path('books/<slug:slug>/', views.books_by_category, name='books-by-category'), 
     path('books/old', views.books_old, name='books_old'),
     path('books/title', views.books_title, name='books_title'),
-    # path('accounts/', include('registration.backends.default.urls'))
+    
    
 
 ]
