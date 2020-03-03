22c44d04388ee27ad1796c75f78ed19e2708ac5c
aselkg <aselekkg@gmail.com>
2020-03-03 13:45:22 -0500

    migration

--- books/settings.py
+++ books/settings.py
@@ -129,7 +129,7 @@ SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-# PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-# STATIC_ROOT = os.path.join(PROJECT_ROOT, ‘static’)
-# MEDIA_ROOT = os.path.join(PROJECT_ROOT, ‘media’)
-# MEDIA_URL = ‘/media/’
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, ‘static’)
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, ‘media’)
+MEDIA_URL = ‘/media/’
